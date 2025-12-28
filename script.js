7ff40be16f178d6b72ef9f5210262add3f4de992
Daniel <ahmagensh@gmail.com>
2025-12-28 08:07:28 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBt2VQ1A3hRFn7nsR-T7kJixB9Gw-M2Gbk",
+  authDomain: "virusgamegl.firebaseapp.com",
+  projectId: "virusgamegl",
+  storageBucket: "virusgamegl.firebasestorage.app",
+  messagingSenderId: "93098679136",
+  appId: "1:93098679136:web:d8964eabdc4dda5510dc05",
+  measurementId: "G-87WNMZ2HJJ"
+};
